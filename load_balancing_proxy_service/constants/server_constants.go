692ad3a7f548868100206318f3a246cb4f258180
Samuel de Carvalho <personal.samuelcarvalho@gmail.com>
2025-08-08 13:27:28 -0300

    Updated

--- load_balancing_proxy_service/constants/server_constants.go
+++ load_balancing_proxy_service/constants/server_constants.go
@@ -3,3 +3,9 @@ package constants
 const PROTOBUF_CONTENT_TYPE_VALUE = "application/x-protobuf"
 
 const NOT_FOUND_ERROR_RESPONSE_MESSAGE = "Not Found!"
+
+const ACCESS_CONTROL_ALLOW_ORIGIN_VALUE = "*"
+
+const ACCESS_CONTROL_ALLOW_METHODS_VALUE = "GET"
+
+const ACCESS_CONTROL_ALLOW_HEADERS_VALUE = "Origin, Content-Type, Accept"
--- load_balancing_proxy_service/controllers/not_found_error_controller.go
+++ load_balancing_proxy_service/controllers/not_found_error_controller.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v3"
 
 	"load_balancing_proxy_service/constants"
@@ -18,9 +20,26 @@ func (notFoundErrorController *NotFoundErrorController) HandleNotFoundErrorRoute
 	context fiber.Ctx) error {
 	notFoundErrorMessageResponse := concerns.GetNotFoundErrorMessageResponse()
 
+	notFoundErrorMessageResponseContentLength := fmt.Sprintf(
+		"%d",
+		len(notFoundErrorMessageResponse))
+
 	context.Set(
 		fiber.HeaderContentType,
 		constants.PROTOBUF_CONTENT_TYPE_VALUE)
+	context.Set(
+		fiber.HeaderContentLength,
+		notFoundErrorMessageResponseContentLength)
+
+	context.Set(
+		fiber.HeaderAccessControlAllowOrigin,
+		constants.ACCESS_CONTROL_ALLOW_ORIGIN_VALUE)
+	context.Set(
+		fiber.HeaderAccessControlAllowMethods,
+		constants.ACCESS_CONTROL_ALLOW_METHODS_VALUE)
+	context.Set(
+		fiber.HeaderAccessControlAllowHeaders,
+		constants.ACCESS_CONTROL_ALLOW_HEADERS_VALUE)
 
 	return context.Status(
 		fiber.StatusNotFound).Send(notFoundErrorMessageResponse)
